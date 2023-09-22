6266baa256b34488bae5dab9bc4f19691c1a1070
Matthew Bomett <bomettmat2@gmail.com>
2023-09-22 16:02:28 +0300

    Done

---
+++ gradeGen.js
@@ -0,0 +1,33 @@
+//import readline to enable input function
+const readline = require('readline')
+
+//create an instance of readline.interface
+const prompt = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+//prompt the user for input
+prompt.question("Enter your grade: ", (grade) => {
+  //grade generator
+  if (grade >= 80 && grade <= 100) {
+    console.log("Your grade is A")
+  }
+  if (grade >= 60 && grade < 80) {
+    console.log("Your grade is B")
+  }
+  if (grade >= 50 && grade < 60) {
+    console.log("Your grade is C")
+  }
+  if (grade >= 40 && grade < 50) {
+    console.log("Your grade is D")
+  }
+  if (grade >= 0 && grade < 40) {
+    console.log("Your grade is E")
+  }
+  else {
+    console.log("INVALID")
+  }
+  //close the readline.interface
+  prompt.close()
+})
