c8971fa5516284bb5f36ec939f5171c744bb6850
Matthew Bomett <bomettmat2@gmail.com>
2023-09-24 13:44:31 +0300

    Done

--- netSalCalc.js
+++ netSalCalc.js
@@ -21,26 +21,52 @@ function calculatePayee(grossIncome) {
   }
   return payee;
 }
-//function to define the NHIF bands and the corresponding deductions
+//function to calculate the NHIF deductions
 function calculateNHIF(grossIncome) {
-  const nhifBands = [6000, 8000, 12000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 60000, 70000, 80000, 90000, 100000];
-  const nhifDeductions = [150, 300, 400, 500, 600, 750, 850, 900, 950, 1000, 1100, 1200, 1300, 1400, 1500, 1600];
+  let NHIF = 0;
 
-  //loop through the NHIF bands to aquire the appropriate deductions
-  for (let i = 0; i < nhifBands.length; i++) {
-    if (grossIncome < nhifBands[i]) {
-    //return the NHIF deductions to their corresponding bands
-      return nhifDeductions[i];
-    }
+  if (grossIncome < 6000 && grossIncome > 0) {
+    NHIF = 150;
+  } else if (grossIncome >= 6000 && grossIncome < 8000) {
+    NHIF = 300;
+  } else if (grossIncome >= 8000 && grossIncome < 12000) {
+    NHIF = 400;
+  } else if (grossIncome >= 12000 && grossIncome < 15000) {
+    NHIF = 500;
+  } else if (grossIncome >= 15000 && grossIncome < 20000) {
+    NHIF = 600;
+  } else if (grossIncome >= 20000 && grossIncome < 25000) {
+    NHIF = 750;
+  } else if (grossIncome >= 25000 && grossIncome < 30000) {
+    NHIF = 850;
+  } else if (grossIncome >= 30000 && grossIncome < 35000) {
+    NHIF = 900;
+  } else if (grossIncome >= 35000 && grossIncome < 40000) {
+    NHIF = 950;
+  } else if (grossIncome >= 40000 && grossIncome < 45000) {
+    NHIF = 1000;
+  } else if (grossIncome >= 45000 && grossIncome < 50000) {
+    NHIF = 1100;
+  } else if (grossIncome >= 50000 && grossIncome < 60000) {
+    NHIF = 1200;
+  } else if (grossIncome >= 60000 && grossIncome < 70000) {
+    NHIF = 1300;
+  } else if (grossIncome >= 70000 && grossIncome < 80000) {
+    NHIF = 1400;
+  } else if (grossIncome >= 80000 && grossIncome < 90000) {
+    NHIF = 1500;
+  } else if (grossIncome >= 90000 && grossIncome < 100000) {
+    NHIF = 1600;
+  } else if (grossIncome >= 100000) {
+    NHIF = 1700;
   }
-  //default NHIF deduction if grossIncome is above the provided bands
-  return 1700;
+  //returning the NHIF deduction to the appropriate conditional statement
+  return NHIF;
 }
-//prompt for input of basic salary and benefits
+
 prompt.question("Enter your basic salary: ", (basicSalary) => {
   prompt.question("Enter your benefits: ", (benefits) => {
     
-    //calculations of variables
     const grossPay = Number(basicSalary) + Number(benefits);
     const deductionNHIF = calculateNHIF(grossPay);
     const deductionNSSF = grossPay * 0.06;
@@ -48,7 +74,6 @@ prompt.question("Enter your basic salary: ", (basicSalary) => {
     const payee = Math.floor(calculatePayee(taxablePay));
     const netPay = grossPay - deductionNHIF - deductionNSSF - payee;
 
-    //display calculations
     console.log(`Payee: ${payee}`);
     console.log(`NHIF Deduction: ${deductionNHIF}`);
     console.log(`NSSF Deduction: ${deductionNSSF}`);
